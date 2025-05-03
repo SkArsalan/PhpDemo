8e08eaf2d022fac002c2bd50f82261fc3eeede3b
SkArsalan <arsalanmech2@gmail.com>
2025-05-03 16:00:51 +0530

    added the index file

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Hello World</title>
+</head>
+<body>
+    <h1><?php echo "Hello, World from PHP!"; ?></h1>
+    <p>Server Time: <?php echo date('Y-m-d H:i:s'); ?></p>
+    <p>Running on: <?php echo $_SERVER['SERVER_SOFTWARE']; ?></p>
+</body>
+</html>
