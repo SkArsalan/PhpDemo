0c2e5ba577424918af823e5a365165331ef7ed46
SkArsalan <arsalanmech2@gmail.com>
2025-05-03 17:01:51 +0530

    chnage

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <title>PHP Hello World</title>
 </head>
 <body>
-    <h1><?php echo "Hello World, from PHP!"; ?></h1>
+    <h1><?php echo "Hello World from PHP!"; ?></h1>
     <p>Server Time: <?php echo date('Y-m-d H:i:s'); ?></p>
     <p>Running on: <?php echo $_SERVER['SERVER_SOFTWARE']; ?></p>
 </body>
